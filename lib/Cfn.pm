268f3cc684cb2b40faf8eff68dd5f9f3c768b6b0
Jose Luis Martínez Torres <joseluis.martinez@capside.com>
2019-03-27 16:00:29 +0100

    Bump version

--- lib/Cfn.pm
+++ lib/Cfn.pm
@@ -882,7 +882,7 @@ package Cfn {
   has Description => (isa => 'Str', is => 'rw');
   has Transform => (isa => 'Cfn::Transform', is => 'rw', coerce => 1);
 
-  our $VERSION = '0.02';
+  our $VERSION = '0.03';
 
   has Parameters => (
     is => 'rw',
