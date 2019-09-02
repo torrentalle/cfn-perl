3d12f4ffb8ebfd7dfa1fc02ee8f8e3f4905ee579
Jose Luis Martinez <joseluis.martinez@capside.com>
2019-09-02 14:44:48 +0200

    Update object model to new regions

--- lib/Cfn/Resource/AWS/Inspector/AssessmentTarget.pm
+++ lib/Cfn/Resource/AWS/Inspector/AssessmentTarget.pm
@@ -1,4 +1,4 @@
-# AWS::Inspector::AssessmentTarget generated from spec 5.0.0
+# AWS::Inspector::AssessmentTarget generated from spec 5.3.0
 use Moose::Util::TypeConstraints;
 
 coerce 'Cfn::Resource::Properties::AWS::Inspector::AssessmentTarget',
@@ -14,7 +14,7 @@ package Cfn::Resource::AWS::Inspector::AssessmentTarget {
     [ 'Arn' ]
   }
   sub supported_regions {
-    [ 'ap-northeast-1','ap-northeast-2','ap-south-1','ap-southeast-2','eu-central-1','eu-north-1','eu-west-1','eu-west-2','us-east-1','us-east-2','us-west-1','us-west-2' ]
+    [ 'ap-northeast-1','ap-northeast-2','ap-south-1','ap-southeast-2','eu-central-1','eu-north-1','eu-west-1','eu-west-2','us-east-1','us-east-2','us-gov-east-1','us-gov-west-1','us-west-1','us-west-2' ]
   }
 }
 
